af3f01e1df77e4dcbf441b5507f47638bdd8b55a
Yosthim <a20201497@pucp.edu.pe>
2024-05-09 19:23:10 -0500

    Proyecto base

---
+++ src/main/java/com/example/gtics_lab6_20201497/GticsLab620201497Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab6_20201497;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab620201497Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GticsLab620201497Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gtics_lab6_20201497/GticsLab620201497ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab6_20201497;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab620201497ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
