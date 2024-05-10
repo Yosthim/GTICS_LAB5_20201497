67c14f475f74e2e12ffba3b16da3ff0fc310586e
Yosthim <a20201497@pucp.edu.pe>
2024-05-09 20:43:10 -0500

    Avance lista tecnicos

--- src/main/java/com/example/gtics_lab6_20201497/controller/TecnicoController.java
+++ src/main/java/com/example/gtics_lab6_20201497/controller/TecnicoController.java
@@ -1,15 +1,28 @@
 package com.example.gtics_lab6_20201497.controller;
 
+import com.example.gtics_lab6_20201497.entity.Technician;
+import com.example.gtics_lab6_20201497.repository.TechnicianRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("Tecnicos")
 public class TecnicoController {
+
+    final TechnicianRepository technicianRepository;
+
+    public TecnicoController(TechnicianRepository technicianRepository) {
+        this.technicianRepository = technicianRepository;
+    }
+
     @GetMapping("")
     public String verListadoTecnicos(Model model) {
+        List<Technician> listaTecnicos = technicianRepository.findAll();
+        model.addAttribute("listaTecnicos", listaTecnicos);
         return "Tecnicos/lista";
     }
 }
